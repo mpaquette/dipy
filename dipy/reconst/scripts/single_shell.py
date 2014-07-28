f240b3c5f0da9683a3f29c5acfb9f78105958a8c
ChantalTax <chantaltax88@gmail.com>
2014-07-28 16:05:14 +0200

    Changed to load MASSIVE data

--- dipy/reconst/scripts/single_shell.py
+++ dipy/reconst/scripts/single_shell.py
@@ -8,7 +8,7 @@ from itertools import product
 
 from dipy.reconst.csdeconv import (auto_response, ConstrainedSphericalDeconvModel,
                                    ConstrainedSDTModel)
-from dipy.reconst.shm import (QballModel, CsaOdfModel)
+from dipy.reconst.shm import (QballModel, CsaOdfModel, lazy_index)
 from dipy.data import get_sphere
 from dipy.reconst.peaks import peaks_from_model, reshape_peaks_for_visualization
 from dipy.reconst.dti import TensorModel
@@ -63,38 +63,44 @@ def screenshot_peaks(peaks_dirs, filename, peaks_values=None, show=False):
 # Load the dataset
 ndir = 60
 shell = None
-               
-# need to write a get_data function    
 
-img = nib.load('Gradient_60.nii')
+# need to write a get_data function
+
+#img = nib.load('Gradient_60.nii')
+directory = 'D:/H_schijf/Data/MASSIVE/Processed/'
+img = nib.load(directory + 'DWIs_A_MD_C_native_rigid_2KS_ordered_shells_48_40_39.nii')
 data = img.get_data()
 affine = img.get_affine()
 
-bvals = np.loadtxt('bvalue.txt')
-bvecs = np.loadtxt('bvec.txt')
+bvals = np.loadtxt(directory + 'DWIs_A_MD_C_native_rigid_2KS_ordered_shells.bval')
+bvecs = np.loadtxt(directory + 'DWIs_A_MD_C_native_rigid_2KS_ordered_shells.bvec')
 
 ind1000 = bvals < 1100
 S1000 = data[..., ind1000]
 bvals = bvals[ind1000]
-bvecs = bvecs[ind1000]
+bvecs = bvecs[:,ind1000]
 
-gtab = gradient_table(bvals, bvecs)
+gtab = gradient_table(bvals=bvals, bvecs=bvecs, big_delta=51.6, small_delta=32.8,
+                      b0_threshold=0.5)
+_where_b0s = lazy_index(gtab.b0s_mask)
+S0 = np.mean(data[:, :, :, _where_b0s])
 
 sh_order = 8
 print("SH order is", sh_order)
-response, ratio = auto_response(gtab, S1000, roi_radius=3, fa_thr=0.9)
+#response, ratio = auto_response(gtab, S1000, roi_radius=1, fa_thr=0.9)
+response = (np.array([0.0015, 0.0003, 0.0003]), S0)
 
 # Compute tensors
 tenmodel = TensorModel(gtab)
 tenfit = tenmodel.fit(S1000)
 sphere = get_sphere('symmetric724').subdivide()
 
-filename = 'test'
+filename = directory + 'test'
 
 # Compute CSD
 S0 = response[1]
-(a, b) = (17., 3.) 
-response = (1e-4 * np.array([a, b, b]), S0)
+(a, b) = (17., 3.)
+#response = (1e-4 * np.array([a, b, b]), S0)
 csd_model = ConstrainedSphericalDeconvModel(gtab, response, sh_order=sh_order)
 peaks_csd = peaks_from_model(model=csd_model,
                              data=S1000,
@@ -112,7 +118,7 @@ nfib = np.sum(np.sum(np.abs(peaks_csd.peak_dirs), axis=-1) > 0, axis=-1).ravel()
 print("1 fiber", np.sum(nfib==1), "2 fibers", np.sum(nfib==2), "3 fibers", np.sum(nfib==3))
 
 screenshot_odf(peaks_csd.odf, sphere, filename + str(a) + "_" + str(b) + "_CSDodf.png", show=True)
-screenshot_peaks(peaks_csd.peak_dirs, filename + str(a) + "_" + str(b) + 
+screenshot_peaks(peaks_csd.peak_dirs, filename + str(a) + "_" + str(b) +
                  "_CSDpeaks.png", peaks_csd.peak_values, show=True)
 # Save everything
 nib.save(nib.Nifti1Image(peaks_csd.shm_coeff.astype('float32'), affine), filename + 'fodf_CSD.nii.gz')
@@ -134,10 +140,10 @@ peaks_qball = peaks_from_model(model=qball_model,
                                return_odf=True,
                                sh_order=sh_order)
 screenshot_odf(peaks_qball.odf, sphere, filename + "_Qballodf.png", show=True)
-screenshot_peaks(peaks_qball.peak_dirs, filename + "_Qballpeaks.png", peaks_qball.peak_values, 
+screenshot_peaks(peaks_qball.peak_dirs, filename + "_Qballpeaks.png", peaks_qball.peak_values,
                  show=True)
 # Compute Qball
-csa_model = CsaOdfModel(gtab, sh_order=sh_order)
+csa_model = CsaOdfModel(gtab, sh_order=4)
 peaks_csa = peaks_from_model(model=csa_model,
                              data=S1000,
                              sphere=sphere,
